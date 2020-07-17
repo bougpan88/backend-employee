3504073396270229155e6489a6bf3e0dc9219a66
takis <fanimatsi@gmail.com>
2020-07-17 10:50:54 +0300

    corrected error that caused null pointer exception

--- src/main/java/com/boug/employee/dto/EmployeeDto.java
+++ src/main/java/com/boug/employee/dto/EmployeeDto.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -28,7 +29,7 @@ public class EmployeeDto {
     private Boolean hasCar;
     @NotNull
     private LocalDate birthDate;
-    private List<EmployeeAttributeDto> employeeAttributes;
+    private List<EmployeeAttributeDto> employeeAttributes = new ArrayList<>();
     private Long supervisorId;
 
 
